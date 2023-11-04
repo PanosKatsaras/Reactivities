14f6c7433134d4b929ed5fe9f77fb810a4fedce3
pan1000 <pan.katsaras@gmail.com>
2023-11-04 14:33:48 +0200

    Axios

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,17 +14,19 @@ interface Props{
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
    
 }
 
-export default function ActivityDashboard({activities, selectedActivity, deleteActivity, 
-    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit}: Props){
+export default function ActivityDashboard({activities, selectedActivity, deleteActivity,
+    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, submitting}: Props){
     return (
         <Grid>
             <GridColumn width="10">
             <ActivityList activities={activities} 
             selectActivity={selectActivity}
-            deleteActivity={deleteActivity} 
+            deleteActivity={deleteActivity}
+            submitting={submitting} 
             />
             </GridColumn>
             <GridColumn width="6">
@@ -35,7 +37,11 @@ export default function ActivityDashboard({activities, selectedActivity, deleteA
                 openForm={openForm}
                 />}
                 {editMode && 
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm closeForm={closeForm} 
+                activity={selectedActivity} 
+                createOrEdit={createOrEdit}
+                submitting={submitting} 
+                />}
             </GridColumn>
         </Grid>
     )
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,10 +6,11 @@ interface Props{
     activity: Activity | undefined;
     closeForm: () => void;
     createOrEdit:(activity: Activity) => void;
+    submitting: boolean;
     
 }
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props){
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props){
 
     const initialState = selectedActivity ?? {
 
@@ -37,12 +38,12 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete="off" >
                 <FormInput placeholder="Title" value={activity.title} name="title" onChange={handleInputChange} />
-                <FormInput placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} />
+                <FormInput type="date" placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} />
                 <Form.TextArea placeholder="Description" value={activity.description} name="description" onChange={handleInputChange} />
                 <FormInput placeholder="Category" value={activity.category} name="category" onChange={handleInputChange} />
                 <FormInput placeholder="City" value={activity.city} name="city" onChange={handleInputChange} />
                 <FormInput placeholder="Venue" value={activity.venue} name="venue" onChange={handleInputChange} />
-                <Button floated="right" positive type="submit" content="Submit" />
+                <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
                 <Button onClick={closeForm} floated="right" type="button" content="Cancel" />
             </Form>
         </Segment>
